008062cc71cf348c05967690762ecf3987d3a546
Raman Mishra <mishraraman455@gmail.com>
2021-01-23 09:16:44 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Discord Bot Gui
+
+Got a Discord bot order but not getting that professional feel yet? <br>
+We got you covered <img src="https://cdn.discordapp.com/emojis/701533218951790643.gif?v=1" alt = "🖥" width="20 px">
+
+# Installation <img src="https://cdn.discordapp.com/emojis/316264057659326464.png?v=1" alt = "🖥" width="35px">
+
+I'll make this part in a second 🚶‍♀️
+
+# Want to contribute? <img src="https://cdn.discordapp.com/emojis/655160803573628928.gif?v=1" alt = "🤔" width="35px">
+
+Just fork the repository, make changes and make a pull request. If the changes are good, then you may become one of the collaborators.
+
+# Like the package? <img src="https://cdn.discordapp.com/emojis/599598716521021441.gif?v=1" width="35px">
+<a href = "https://www.buymeacoffee.com/TheRamann">
+Support the creator here
+</a>
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Process processss;
+        bool BotOn = false;
+        ProcessStartInfo p = new ProcessStartInfo();
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            p.FileName = "cmd.exe";
+            p.WindowStyle = ProcessWindowStyle.Hidden;
+            p.Arguments = "/k node index.js";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (BotOn == false) 
+            {
+                processss = Process.Start(p);
+                BotOn = true;
+                MessageBox.Show("Bot Is Now Online");
+            }
+            else
+            {
+                MessageBox.Show("Bot Is Already online");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (BotOn == true) 
+            {
+                processss.Kill();
+                BotOn = false;
+                MessageBox.Show("Bot Is Now Offline");
+            }
+            else
+            {
+                MessageBox.Show("Bot was not online");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start("notepad.exe", "./bot/token.json");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
